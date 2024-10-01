b9f448e95e88ab6b0600290782457d9b41c6f1da
Mark Slorach <hello@markslorach.com>
2024-10-01 21:58:01 +0100

    Initial setup

---
+++ app/components/Count.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+const Count = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <React.Fragment>
+      <p className="mb-2 text-5xl font-bold">{count}</p>
+      <Button
+        onClick={handleClick}
+        variant="secondary"
+        className="font-semibold"
+      >
+        Count
+      </Button>
+    </React.Fragment>
+  );
+};
+
+export default Count;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,12 +9,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "1.5rem",
+        lg: "2rem",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
